a6eb032dad71de85de4ea8f51d787b27b4009e39
Valentin Podkamennyi <valentin@dtm.io>
2017-10-27 16:51:50 -0700

    Updated manifest.json

--- worker.js
+++ worker.js
@@ -4,6 +4,7 @@
  * @see http://google.github.io/styleguide/javascriptguide.xml
  * @see http://developers.google.com/closure/compiler/docs/js-for-compiler
  * @see https://developers.google.com/web/fundamentals/primers/service-workers/
+ * @see https://developers.google.com/web/fundamentals/app-install-banners/
  */
 
 /** @const {string} */ var CACHE_NAME = 'komito-cache-v1';
@@ -19,15 +20,24 @@
   '/dashboard/dashboard.js'
 ];
 
+var SHOW_ALERT = ~location.search.indexOf('alert');
+
 self.addEventListener('install', function(event) {
+  (SHOW_ALERT ? alert : console.log)('worker:install');
   event.waitUntil(caches.open(CACHE_NAME).then(function(cache) {
-    console.log('Opened cache');
+    (SHOW_ALERT ? alert : console.log)('worker:install:cache');
     return cache.addAll(CACHE_URLS);
   }));
 });
 
+self.addEventListener('activate', function(event) {
+  (SHOW_ALERT ? alert : console.log)('worker:activate');
+});
+
 self.addEventListener('fetch', function(event) {
+  (SHOW_ALERT ? alert : console.log)('worker:fetch');
   event.respondWith(caches.match(event.request).then(function(response) {
+  (SHOW_ALERT ? alert : console.log)('worker:fetch:match');
     return response || fetch(event.request);
   }));
 });
