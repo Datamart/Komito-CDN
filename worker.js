d86c7fece37aa62fdcaa2300c9af0b93f208bc5f
Valentin Podkamennyi <valentin@dtm.io>
2017-10-28 10:07:47 -0700

    Updated service worker.

--- worker.js
+++ worker.js
@@ -45,6 +45,15 @@ self.addEventListener('fetch', function(event) {
 
 self.addEventListener('activate', function(event) {
   log_('worker:activate');
+  var whitelist = [CACHE_NAME];
+
+  event.waitUntil(caches.keys().then(function(cacheNames) {
+    return Promise.all(cacheNames.map(function(cacheName) {
+      if (whitelist.indexOf(cacheName) === -1) {
+        return caches.delete(cacheName);
+      }
+    }));
+  }));
 });
 
 self.addEventListener('beforeinstallprompt', function(event) {
