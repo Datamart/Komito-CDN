c1b3b7c51410da188131ba2a52241491c5fa28b9
Valentin Podkamennyi <valentin@dtm.io>
2017-12-26 17:45:57 -0800

    Updated initial support page.

--- worker.js
+++ worker.js
@@ -6,7 +6,7 @@
  * @see https://developers.google.com/web/fundamentals/primers/service-workers/
  */
 
-/** @const {string} */ var CACHE_KEY = 'komito-cache-20171219-1045';
+/** @const {string} */ var CACHE_KEY = 'komito-cache-20171226-1745';
 /** @const {string} */ var SCOPE_URL = 'https://komito.net/';
 
 /** @const {Array.<string>} */ var CACHE_URLS = [
