a0cb248279c28dec532d1f201888296d17631052
Valentin Podkamennyi <valentin@dtm.io>
2018-07-05 12:51:41 -0700

    Updated service worker cache key.

--- worker.js
+++ worker.js
@@ -6,7 +6,7 @@
  * @see https://developers.google.com/web/fundamentals/primers/service-workers/
  */
 
-/** @const {string} */ var CACHE_KEY = 'komito-cache-20180517-1630';
+/** @const {string} */ var CACHE_KEY = 'komito-cache-20180705-1250';
 /** @const {string} */ var SCOPE_URL = 'https://komito.net/';
 /** @const {string} */ var WORKER_JS = 'worker.js';
 
