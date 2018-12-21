f4a912f095ef50d3f0b501132e380e04083c4723
Valentin Podkamennyi <valentin@dtm.io>
2018-12-20 21:39:31 -0800

    Updated service worker version.

--- worker.js
+++ worker.js
@@ -6,7 +6,7 @@
  * @see https://developers.google.com/web/fundamentals/primers/service-workers/
  */
 
-/** @const {string} */ var CACHE_KEY = 'komito-cache-20181206-0930';
+/** @const {string} */ var CACHE_KEY = 'komito-cache-20181220-2130';
 /** @const {string} */ var SCOPE_URL = 'https://komito.net/';
 /** @const {string} */ var WORKER_JS = 'worker.js';
 
