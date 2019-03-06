505b55c9c4bd1c58e1bd13c43c9ebd5d7399e8c1
Valentin Podkamennyi <valentin@dtm.io>
2019-03-05 22:10:39 -0800

    Updated test page for future posts.

--- worker.js
+++ worker.js
@@ -6,7 +6,7 @@
  * @see https://developers.google.com/web/fundamentals/primers/service-workers/
  */
 
-/** @const {string} */ var CACHE_KEY = 'komito-cache-20190305-1005';
+/** @const {string} */ var CACHE_KEY = 'komito-cache-20190305-1010';
 /** @const {string} */ var SCOPE_URL = 'https://komito.net/';
 /** @const {string} */ var WORKER_JS = 'worker.js';
 
