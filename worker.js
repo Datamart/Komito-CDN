a0502b2234057e9ee579cfc400e1c260d8742650
Valentin Podkamennyi <valentin@dtm.io>
2019-05-23 18:06:17 -0700

    Fixed Facebook tracker.

--- worker.js
+++ worker.js
@@ -6,7 +6,7 @@
  * @see https://developers.google.com/web/fundamentals/primers/service-workers/
  */
 
-/** @const {string} */ var CACHE_KEY = 'komito-cache-20190506-1730';
+/** @const {string} */ var CACHE_KEY = 'komito-cache-20190523-1800';
 /** @const {string} */ var SCOPE_URL = 'https://komito.net/';
 /** @const {string} */ var WORKER_JS = 'worker.js';
 
