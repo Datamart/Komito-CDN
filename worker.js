9474dde78163dd17c940f5a13e5bf3b3373420b2
Valentin Podkamennyi <valentin@dtm.io>
2019-06-14 18:17:29 -0700

    Updated  to required origins

--- worker.js
+++ worker.js
@@ -6,7 +6,7 @@
  * @see https://developers.google.com/web/fundamentals/primers/service-workers/
  */
 
-/** @const {string} */ var CACHE_KEY = 'komito-cache-20190614-1620';
+/** @const {string} */ var CACHE_KEY = 'komito-cache-20190614-1815';
 /** @const {string} */ var SCOPE_URL = 'https://komito.net/';
 /** @const {string} */ var WORKER_JS = 'worker.js';
 
