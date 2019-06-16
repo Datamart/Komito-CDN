5b8e7fff2e29f0a24bd3951512b6bc8b346db855
Valentin Podkamennyi <valentin@dtm.io>
2019-06-15 17:46:19 -0700

    Added webp images.

--- worker.js
+++ worker.js
@@ -6,7 +6,7 @@
  * @see https://developers.google.com/web/fundamentals/primers/service-workers/
  */
 
-/** @const {string} */ var CACHE_KEY = 'komito-cache-20190615-1540';
+/** @const {string} */ var CACHE_KEY = 'komito-cache-20190615-1745';
 /** @const {string} */ var SCOPE_URL = 'https://komito.net/';
 /** @const {string} */ var WORKER_JS = 'worker.js';
 
