8707cbfbc220e594b9685a6c3a82a29085ec7f8d
Valentin Podkamennyi <valentin@dtm.io>
2019-07-22 23:09:25 -0700

    Added link to NPM package.

--- worker.js
+++ worker.js
@@ -6,7 +6,7 @@
  * @see https://developers.google.com/web/fundamentals/primers/service-workers/
  */
 
-/** @const {string} */ var CACHE_KEY = 'komito-cache-20190714-1115';
+/** @const {string} */ var CACHE_KEY = 'komito-cache-20190722-2305';
 
 /** @const {!Array.<string>} */ var CACHE_URLS = [
   // Assets:
