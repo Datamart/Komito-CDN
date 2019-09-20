05260e0f2c3eaa0956ef7dbd1d01a5b2ae82e705
Valentin Podkamennyi <valentin@dtm.io>
2019-09-20 07:19:50 -0700

    Updated about page.

--- worker.js
+++ worker.js
@@ -6,7 +6,7 @@
  * @see https://developers.google.com/web/fundamentals/primers/service-workers/
  */
 
-/** @const {string} */ var CACHE_KEY = 'komito-cache-20190920-0715';
+/** @const {string} */ var CACHE_KEY = 'komito-cache-20190920-0720';
 
 /** @const {!Array.<string>} */ var CACHE_URLS = [
   // Assets:
