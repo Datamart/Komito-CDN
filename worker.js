f9158a26c0035c0330740bbf884331d96a9d7aaa
Valentin Podkamennyi <vpodk@users.noreply.github.com>
2020-09-12 12:46:53 -0700

    Updated CACHE_KEY

--- worker.js
+++ worker.js
@@ -6,7 +6,7 @@
  * @see https://developers.google.com/web/fundamentals/primers/service-workers/
  */
 
-/** @const {string} */ var CACHE_KEY = 'komito-cache-20200409-1330';
+/** @const {string} */ var CACHE_KEY = 'komito-cache-20200912-1245';
 
 /** @const {!Array.<string>} */ var CACHE_URLS = [
   // Assets:
