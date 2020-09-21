4d1962660b041dcf527349df13823ec82bbcd17b
Valentin Podkamennyi <vpodk@users.noreply.github.com>
2020-09-20 20:38:36 -0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -6,7 +6,7 @@
  * @see https://developers.google.com/web/fundamentals/primers/service-workers/
  */
 
-/** @const {string} */ var CACHE_KEY = 'komito-cache-20200920-2000';
+/** @const {string} */ var CACHE_KEY = 'komito-cache-20200920-2030';
 
 /** @const {!Array.<string>} */ var CACHE_URLS = [
   // Assets:
