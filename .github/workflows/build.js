be9f0adacb4fe3955ee70fabc1b469f81d4d6798
Valentin Podkamennyi <vpodkamenyi@gmail.com>
2022-11-13 21:40:44 -0800

    Updated gh-pages-build-deploy

--- .github/workflows/build.js
+++ .github/workflows/build.js
@@ -1,5 +1,20 @@
 const fs = require("fs");
+const path = require("path");
 const { resolve } = require("path");
+
+const ROOT = resolve(__dirname + "/../../");
+
+const HEADER_JS = `${ROOT}/assets/scripts/header.js`;
+const HEADER_RE =
+  /<script src="[\/\.]+assets\/scripts\/header\.js"(\s+async)?><\/script>/gm;
+
+const FOOTER_JS = `${ROOT}/assets/scripts/footer.js`;
+const FOOTER_RE =
+  /<script src="[\/\.]+assets\/scripts\/footer\.js"(\s+async)?><\/script>/gm;
+
+const CTA_AREA_JS = `${ROOT}/assets/scripts/cta-area.js`;
+const CTA_AREA_RE =
+  /<script src="[\/\.]+assets\/scripts\/cta-area\.js"(\s+async)?><\/script>/gm;
 
 globalThis.window = {};
 globalThis.document = {};
@@ -10,38 +25,17 @@ globalThis.getNavMenu = () => globalThis.window.getNavMenu();
 const buildSection = (filePath, regExp, scriptPath) => {
   const pathname = filePath.split("/").slice(-2, -1)[0];
   globalThis.location.pathname =
-    "Komito-CDN" === pathname ? "/" : "/" + pathname + "/";
+    "Komito-CDN" === pathname ? "/" : `/${pathname}/`;
 
   globalThis.insertAdjacentHTMLContent = (_, content) => {
     const data = fs.readFileSync(filePath, "utf8");
     const result = data.replace(regExp, content.replace(/\s+/gm, " "));
     fs.writeFileSync(filePath, result, "utf8");
   };
+
   require(scriptPath);
   delete require.cache[require.resolve(scriptPath)];
 };
-
-const FILES = [
-  resolve(__dirname + "/../../index.html"),
-  resolve(__dirname + "/../../about/index.html"),
-  resolve(__dirname + "/../../demo/index.html"),
-  resolve(__dirname + "/../../integration/index.html"),
-  resolve(__dirname + "/../../services/index.html"),
-  resolve(__dirname + "/../../support/index.html"),
-  resolve(__dirname + "/../../404.html"),
-];
-
-const HEADER_JS = resolve(__dirname + "/../../assets/scripts/header.js");
-const HEADER_RE =
-  /<script src="[\/\.]+assets\/scripts\/header\.js"(\s+async)?><\/script>/gm;
-
-const FOOTER_JS = resolve(__dirname + "/../../assets/scripts/footer.js");
-const FOOTER_RE =
-  /<script src="[\/\.]+assets\/scripts\/footer\.js"(\s+async)?><\/script>/gm;
-
-const CTA_AREA_JS = resolve(__dirname + "/../../assets/scripts/cta-area.js");
-const CTA_AREA_RE =
-  /<script src="[\/\.]+assets\/scripts\/cta-area\.js"(\s+async)?><\/script>/gm;
 
 const buildHeader = (filePath) => {
   buildSection(filePath, HEADER_RE, HEADER_JS);
@@ -55,7 +49,24 @@ const buildCTA = (filePath) => {
   buildSection(filePath, CTA_AREA_RE, CTA_AREA_JS);
 };
 
-FILES.forEach((filePath) => {
+const getFiles = (dirPath) => {
+  let result = [];
+  const files = fs.readdirSync(dirPath);
+  for (var i = 0; i < files.length; i++) {
+    var filename = path.join(dirPath, files[i]);
+    var stat = fs.lstatSync(filename);
+    if (stat.isDirectory()) {
+      getFiles(filename);
+      result = result.concat(getFiles(filename));
+    } else if (filename.endsWith(".html")) {
+      result.push(filename);
+    }
+  }
+  return result;
+};
+
+const files = getFiles(ROOT);
+files.forEach((filePath) => {
   buildHeader(filePath);
   buildFooter(filePath);
   buildCTA(filePath);
